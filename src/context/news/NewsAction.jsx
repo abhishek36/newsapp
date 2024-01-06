a498112022250e2dcb05d7630f9cd6f5554ac1f0
abhishek sharma <abhi365ddd@gmail.com>
2024-01-06 15:30:01 +0530

    changes

--- src/context/news/NewsAction.jsx
+++ src/context/news/NewsAction.jsx
@@ -1,6 +1,6 @@
 export const fetchnews = async (topic) => {
   const response =
-    await fetch(`https://newsapi.org/v2/everything?q=%${topic}&from=2024-1-2&sortBy=publishedAt&apiKey=20d5bb0f2e30448992f9e41fdad5d22e
+    await fetch(`https://newsapi.org/v2/everything?q=%${topic}&from=2024-01-1&sortBy=publishedAt&apiKey=20d5bb0f2e30448992f9e41fdad5d22e
     `);
   const data = await response.json();
   return data.articles;
